d7678a3b180785c69293428428d3ae2da053805c
Sohelrana2815 <sohel152302@gmail.com>
2026-02-27 22:32:50 +0600

    fix some padding & margin

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import SmoothScrolling from "./components/layout/SmoothScroll";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SmoothScrolling>
       <RouterProvider router={router} />
-    </SmoothScrolling>
   </StrictMode>,
 );
